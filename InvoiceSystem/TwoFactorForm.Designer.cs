04cecf9e96a1da32f5cb03ce4a9fc09380256e7d
shkraiev <shkraev07@gmail.com>
2026-01-11 21:58:44 +0100

    Two factor form UI designer code

---
+++ InvoiceSystem/TwoFactorForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace InvoiceSystem
+{
+    partial class TwoFactorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kod 2FA";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(12, 25);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(199, 20);
+            this.txtCode.TabIndex = 1;
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Location = new System.Drawing.Point(12, 60);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(199, 23);
+            this.btnVerify.TabIndex = 2;
+            this.btnVerify.Text = "Potwierdź";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // TwoFactorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 108);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(239, 147);
+            this.MinimumSize = new System.Drawing.Size(239, 147);
+            this.Name = "TwoFactorForm";
+            this.Text = "TwoFactorForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnVerify;
+    }
+}
