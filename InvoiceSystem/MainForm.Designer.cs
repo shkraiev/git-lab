c5da3498549e0134f6635980c8c28737c9c2232c
shkraiev <shkraev07@gmail.com>
2026-01-11 21:50:24 +0100

    Main form UI designer code

---
+++ InvoiceSystem/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace InvoiceSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.btnContractors = new System.Windows.Forms.Button();
+            this.btnInvoices = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(13, 13);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(35, 13);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "label1";
+            // 
+            // btnContractors
+            // 
+            this.btnContractors.Location = new System.Drawing.Point(12, 40);
+            this.btnContractors.Name = "btnContractors";
+            this.btnContractors.Size = new System.Drawing.Size(181, 23);
+            this.btnContractors.TabIndex = 1;
+            this.btnContractors.Text = "Kontrahenci";
+            this.btnContractors.UseVisualStyleBackColor = true;
+            this.btnContractors.Click += new System.EventHandler(this.btnContractors_Click);
+            // 
+            // btnInvoices
+            // 
+            this.btnInvoices.Location = new System.Drawing.Point(12, 85);
+            this.btnInvoices.Name = "btnInvoices";
+            this.btnInvoices.Size = new System.Drawing.Size(181, 23);
+            this.btnInvoices.TabIndex = 2;
+            this.btnInvoices.Text = "Faktury";
+            this.btnInvoices.UseVisualStyleBackColor = true;
+            this.btnInvoices.Click += new System.EventHandler(this.btnInvoices_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(12, 134);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(181, 23);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Wyloguj";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 195);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnInvoices);
+            this.Controls.Add(this.btnContractors);
+            this.Controls.Add(this.lblUser);
+            this.MaximumSize = new System.Drawing.Size(221, 234);
+            this.MinimumSize = new System.Drawing.Size(221, 234);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Button btnContractors;
+        private System.Windows.Forms.Button btnInvoices;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
