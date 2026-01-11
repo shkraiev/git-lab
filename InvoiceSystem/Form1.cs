0c85cedf9e9b4e506aed3ac3f2269abb0e05cf16
shkraiev <shkraev07@gmail.com>
2026-01-11 21:52:38 +0100

    Strona logowania logic

---
+++ InvoiceSystem/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace InvoiceSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btnOpenRegister_Click(object sender, EventArgs e)
+        {
+            new RegisterForm().ShowDialog();
+        }
+
+        private void btnOpenLogin_Click(object sender, EventArgs e)
+        {
+            var lf = new LoginForm();
+            lf.ShowDialog();
+
+            if (Session.IsLoggedIn)
+            {
+                
+                this.Hide();
+                var mf = new MainForm();
+                mf.FormClosed += (s, args) => this.Close(); 
+                mf.Show();
+            }
+        }
+
+        
+    }
+}
